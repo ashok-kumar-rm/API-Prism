887f39706732183a47181179cb4643a50d12ce68
Nambi <seenivasanv50@gmail.com>
2025-08-29 21:31:12 +0530

    Rest API to get payload data

--- src/main/java/org/apitest/apiprism/controller/ApiPrismController.java
+++ src/main/java/org/apitest/apiprism/controller/ApiPrismController.java
@@ -1,20 +1,38 @@
 package org.apitest.apiprism.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apitest.apiprism.intf.ApiPrismInterface;
-import org.apitest.apiprism.service.ApiPrismService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apitest.apiprism.transferobjects.InputPayload;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
+@Slf4j
 public class ApiPrismController {
 
     private final ApiPrismInterface apiPrismService;
 
-    @GetMapping("/api/prism")
+    @GetMapping("/prism")
     public String index() {
         return apiPrismService.index();
+    }
+
+    @PostMapping("/test")
+    public ResponseEntity<String> test(@Valid @RequestBody InputPayload inputPayload){
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String response = objectMapper.writeValueAsString(apiPrismService.index());
+            log.info("Tested Successfully");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }catch (Exception e){
+            log.error("Error while testing api with path {}, ERROR : {}", inputPayload.getUri(), e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
