3a79929c9b992b7c3a8e8e1998a20e8251f47443
Nambi <seenivasanv50@gmail.com>
2025-08-29 21:27:44 +0530

    Data object for payload

---
+++ src/main/java/org/apitest/apiprism/transferobjects/InputPayload.java
@@ -0,0 +1,28 @@
+package org.apitest.apiprism.transferobjects;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InputPayload {
+    @JsonProperty("uri")
+    @NotBlank(message = "Api Uri cannot be empty")
+    private String uri;
+
+    @JsonProperty("method")
+    @NotBlank(message = "Method cannot be empty")
+    private String method;
+
+    @JsonProperty("payload")
+    private Map<String,Object> payload;
+
+}
